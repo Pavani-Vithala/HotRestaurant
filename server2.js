81010f9a471b259e8b552b53efd2f97a278b0206
Pavani Vithala <pavani.vithala18@gmail.com>
2018-12-13 20:30:59 -0500

    Added all table and waitlist

--- server2.js
+++ server2.js
@@ -15,70 +15,83 @@ app.use(express.json());
 // Star Wars Characters (DATA)
 // =============================================================
 var clients = [
- {
+    {
 
-   name: "Adam",
-   phoneNumber: 437567772,
-   email: "adam@gmail.com",
-   id: 1,
- },
+        name: "Adam",
+        phoneNumber: 437567772,
+        email: "adam@gmail.com",
+        id: 1,
+    },
 
- {
+    {
 
-   name: "Sophie",
-   phoneNumber: 437987123,
-   email: "sophie@gmail.com",
-   id: 2,
- },
- {
+        name: "Sophie",
+        phoneNumber: 437987123,
+        email: "sophie@gmail.com",
+        id: 2,
+    },
+    {
 
-   name: "Sam",
-   phoneNumber: 437233432,
-   email: "sam@gmail.com",
-   id: 3,
- }
+        name: "Sam",
+        phoneNumber: 437233432,
+        email: "sam@gmail.com",
+        id: 3,
+    }
 ];
 
+var waitingList = [];
 
 //Initial page routing
-  app.get("/",function(req,res){
-      res.sendFile(path.join(__dirname,"home.html"));
-  });
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
 
-  app.get("/tables", function(req,res){
-      res.sendFile(path.join(__dirname, "tables.html"));
-  });
-  app.get("/reserve", function(req,res){
-      res.sendFile(path.join(__dirname, "reserve.html"))
-  });
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"))
+});
 
-  
 
-  //To display the details of all the tables
 
-  app.get("/api/tables", function(req,res){
-      return res.json(clients);
-  })
+//To display the details of all the tables
 
+app.get("/api/tables", function (req, res) {
+    return res.json(clients);
+})
+
+
+//To display the details of all waitlist tables
+
+app.get("/api/waitlist", function (req, res) {
+    return res.json(waitingList);
+})
 // To post the table details when a reservation is made
 
-app.post("/reserve", function(req, res) {
+app.post("/reserve", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newReservation = req.body;
-  
+
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newReservation.Name = newReservation.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newReservation);
-  
-    clients.push(newReservation);
-  
-    res.json(newReservation);
-  });
+
+    if (clients.length < 6) {
+        newReservation.Name = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+        console.log(newReservation);
+
+        clients.push(newReservation);
+
+        res.json(newReservation);
+    } else {
+        waitingList.push(newReservation);
+    }
+
+});
 
 //To listen to the local port
-  app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
